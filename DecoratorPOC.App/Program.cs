5ae229449254f7f05dc8e45b78e8331cdbe2dfc5
Bill Blankenship <Bill Blankenship>
2017-09-09 10:07:29 -0400

    Add project files.

---
+++ DecoratorPOC.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DecoratorPOC.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
