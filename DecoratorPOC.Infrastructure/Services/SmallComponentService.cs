d1d72f4f60892fff66aa1a54ec89a31fc40c4842
William Blankenship <blankenship.william@gmail.com>
2017-09-16 07:20:48 -0400

    Version updates Service class added

---
+++ DecoratorPOC.Infrastructure/Services/SmallComponentService.cs
@@ -0,0 +1,12 @@
+﻿using DecoratorPOC.Core.Models;
+
+namespace DecoratorPOC.Infrastructure.Services
+{
+    public class SmallComponentService
+    {
+        public SmallComponent GetSmallComponent()
+        {
+            return new SmallComponent();
+        }
+    }
+}
---
+++ DecoratorPOC.Tests/SmallComponentTests.cs
@@ -0,0 +1,22 @@
+﻿using DecoratorPOC.Infrastructure.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorPOC.Tests
+{
+    [TestClass]
+    public class Small_Component_Service_Should
+    {
+        [TestMethod]
+        public void Return_Valid_Small_Component()
+        {
+            var svc = new SmallComponentService();
+            var smallComponent = svc.GetSmallComponent();
+
+            Assert.AreEqual(3.01m, smallComponent.GetCost());
+            Assert.AreEqual("Small component", smallComponent.GetDescription());
+        }
+    }
+}
